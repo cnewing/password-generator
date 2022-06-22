5bd43b153c9c26c3cfc8133b1a099cbdfe8b5ee6
Christa <christanewing47@gmail.com>
2022-06-22 12:02:01 -0400

    create variable functions

--- script.js
+++ script.js
@@ -1,8 +1,61 @@
-// Assignment code here
-
+var uppercase = ["A", "B", "C", "D"];
+var lowercase = ["a", "b", "c", "d"];
+var number = ["1", "2", "3", "4", "5"];
+var symbol = ["!", "@", "#", "$"];
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+function generatePassword() {
+  alert("Enter password info in the follow prompts");
+  var passwordLength = prompt(
+    "How many characters would you like your password to have?"
+  );
+
+  if (isNaN(passwordLength)) {
+    alert("password length must be a number!");
+    return;
+  }
+
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert("Password must be between 8 & 128 characters!");
+    return;
+  }
+
+  var useUpperCase = confirm("Do you want uppercase letters?");
+  var useLowerCase = confirm("Do you want lowercase letters?");
+  var useNumbers = confirm("Do you want numbers?");
+  var useSymbols = confirm("Do you want symbols?");
+
+  if (
+    useUpperCase === false &&
+    useLowerCase === false &&
+    useNumbers === false &&
+    useSymbols === false
+  ) {
+    alert("Must choose at least one character type");
+    return;
+  }
+  var userCharacters = [];
+
+  if (useUpperCase === true) {
+    userCharacters = userCharacters.concat(uppercase);
+  }
+
+  if (useLowerCase === true) {
+    userCharacters = userCharacters.concat(lowercase);
+  }
+
+  if (useNumbers === true) {
+    userCharacters = userCharacters.concat(number);
+  }
+
+  if (useSymbols === true) {
+    userCharacters = userCharacters.concat(symbol);
+  }
+
+  console.log(userCharacters);
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -10,7 +63,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
